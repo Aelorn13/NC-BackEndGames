75c598f67859fd8ed069c42c3b5a036ab3082e43
aelorn13 <a.maslov364@gmail.com>
2022-11-14 12:27:25 +0000

    remove express part for posting/patching

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const { getCategories } = require("./controllers/categories.js");
 
-const app = express();
-
-app.use(express.json());
-
 app.get("/api/categories", getCategories);
 
 module.exports = app;
