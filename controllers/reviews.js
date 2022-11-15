d1629bf6f45517ad2d19f33fcc40e150fc64b007
aelorn13 <a.maslov364@gmail.com>
2022-11-15 14:44:29 +0000

    add patch request and required tests

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,6 +3,7 @@ const {
   fetchReviewById,
   fetchReviewCommentsById,
   insertComment,
+  updateReview,
 } = require("../models/reviews");
 
 exports.getReviews = (req, res) => {
@@ -37,6 +38,17 @@ exports.postComment = (req, res, next) => {
   insertComment(comment, review_id)
     .then((newComment) => {
       res.status(201).send({ newComment: newComment });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+exports.patchReview = (req, res, next) => {
+  const { inc_votes } = req.body;
+  const { review_id } = req.params;
+  updateReview(inc_votes, review_id)
+    .then((updatedReview) => {
+      res.status(200).send({ updatedReview });
     })
     .catch((err) => {
       next(err);
