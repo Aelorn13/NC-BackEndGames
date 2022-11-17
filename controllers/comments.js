cdb7a21e5878a446feeae18de25c22ba11c106aa
aelorn13 <a.maslov364@gmail.com>
2022-11-17 14:12:30 +0000

    add patch request for comments/:comment_id

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,9 +1,20 @@
-const { removeCommentById } = require("../models/comments");
+const { removeCommentById, updateCommentById } = require("../models/comments");
 exports.deleteCommentById = (req, res, next) => {
   const { comment_id } = req.params;
   removeCommentById(comment_id)
     .then(() => {
       res.status(204).send();
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+exports.patchCommentById = (req, res, next) => {
+  const { inc_votes } = req.body;
+  const { comment_id } = req.params;
+  updateCommentById(inc_votes, comment_id)
+    .then((comment) => {
+      res.status(200).send({ comment });
     })
     .catch((err) => {
       next(err);
--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,12 @@
 const commentsRouter = require("express").Router();
-const { deleteCommentById } = require("../controllers/comments.js");
+const {
+  deleteCommentById,
+  patchCommentById,
+} = require("../controllers/comments.js");
 
-commentsRouter.route("/:comment_id").delete(deleteCommentById);
+commentsRouter
+  .route("/:comment_id")
+  .delete(deleteCommentById)
+  .patch(patchCommentById);
 
 module.exports = commentsRouter;
